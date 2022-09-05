abdce089056f7ad57edf4ebe686e7038d9059895
gabriellukke <gabriellukke@outlook.com>
2022-09-05 17:43:38 -0300

    test: constants refact

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,58 +1,81 @@
 /// <reference types="cypress" />
 
+import {
+  LOGIN_EMAIL_TEST_ID,
+  LOGIN_PASSWORD_TEST_ID,
+  LOGIN_SUBMIT_TEST_ID,
+  LOGIN_URL, REGISTER_LINK_TEST_ID,
+  WALL_LINK_TEST_ID
+} from '../utils/constants';
+
 describe('Login page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
     cy.clearLocalStorage();
   });
 
   it('Should not be able to submit login with invalid empty fields', () => {
-    cy.get('[data-testid="login-submit"]').click();
+    cy.visit(LOGIN_URL);
+
+    cy.get(LOGIN_SUBMIT_TEST_ID).click();
     cy.contains('Fields cannot be blank');
   });
   
   it('Should not be able to submit login with invalid email pattern', () => {
-    cy.get('[data-testid="login-email"]').type('gabs');
-    cy.get('[data-testid="login-password"]').type('12345678');
-    cy.get('[data-testid="login-submit"]').click();
+    cy.visit(LOGIN_URL);
+
+    cy.get(LOGIN_EMAIL_TEST_ID).type('gabs');
+    cy.get(LOGIN_PASSWORD_TEST_ID).type('12345678');
+    cy.get(LOGIN_SUBMIT_TEST_ID).click();
     cy.contains('Email is not valid');
   });
   
   it('Should not be able to submit login with password below the minimun length', () => {
-    cy.get('[data-testid="login-email"]').type('gabs@test.com');
-    cy.get('[data-testid="login-password"]').type('123');
-    cy.get('[data-testid="login-submit"]').click();
+    cy.visit(LOGIN_URL);
+
+    cy.get(LOGIN_EMAIL_TEST_ID).type('gabs@test.com');
+    cy.get(LOGIN_PASSWORD_TEST_ID).type('123');
+    cy.get(LOGIN_SUBMIT_TEST_ID).click();
     cy.contains('Password must be at least 8 characters');
   });
   
   it('Should not be able to login with a non-existent user', () => {
-    cy.get('[data-testid=login-email]').type('xablau@test.com');
-    cy.get('[data-testid=login-password]').type('xablau1234');
-    cy.get('[data-testid=login-submit]').click();
+    cy.visit(LOGIN_URL);
+
+    cy.get(LOGIN_EMAIL_TEST_ID).type('xablau@test.com');
+    cy.get(LOGIN_PASSWORD_TEST_ID).type('xablau1234');
+    cy.get(LOGIN_SUBMIT_TEST_ID).click();
     cy.contains('Unauthorized, incorrect or unregistered email');
   });
 
   it('Should not be able to login with wrong user password', () => {
-    cy.get('[data-testid=login-email]').type('gabs@test.com');
-    cy.get('[data-testid=login-password]').type('xablau1234');
-    cy.get('[data-testid=login-submit]').click();
+    cy.visit(LOGIN_URL);
+
+    cy.get(LOGIN_EMAIL_TEST_ID).type('gabs@test.com');
+    cy.get(LOGIN_PASSWORD_TEST_ID).type('xablau1234');
+    cy.get(LOGIN_SUBMIT_TEST_ID).click();
     cy.contains('Unauthorized, incorrect password');
   });
 
   it('Should be able to login', () => {
-    cy.get('[data-testid=login-email]').type('gabs@test.com');
-    cy.get('[data-testid=login-password]').type('12345678');
-    cy.get('[data-testid=login-submit]').click();
+    cy.visit(LOGIN_URL);
+
+    cy.get(LOGIN_EMAIL_TEST_ID).type('gabs@test.com');
+    cy.get(LOGIN_PASSWORD_TEST_ID).type('12345678');
+    cy.get(LOGIN_SUBMIT_TEST_ID).click();
     cy.location('pathname').should('eq', '/wall');
   });
 
   it('should be able to access register page', () => {
-    cy.get('[data-testid=register-link]').click();
+    cy.visit(LOGIN_URL);
+
+    cy.get(REGISTER_LINK_TEST_ID).click();
     cy.location('pathname').should('eq', '/register');
   });
 
   it('should be able to access the wall page as a guest', () => {
-    cy.get('[data-testid=wall-link]').click();
+    cy.visit(LOGIN_URL);
+
+    cy.get(WALL_LINK_TEST_ID).click();
     cy.location('pathname').should('eq', '/wall');
   });
 });
---
+++ cypress/utils/constants.js
@@ -0,0 +1,11 @@
+export const BASE_URL = 'http://localhost:3000';
+export const LOGIN_URL = `${BASE_URL}/`;
+export const REGISTER_URL = `${BASE_URL}/register`;
+export const WALL_URL = `${BASE_URL}/wall`;
+
+export const LOGIN_EMAIL_TEST_ID = '[data-testid="login-email"]';
+export const LOGIN_PASSWORD_TEST_ID = '[data-testid="login-password"]';
+export const LOGIN_SUBMIT_TEST_ID = '[data-testid="login-submit"]';
+
+export const REGISTER_LINK_TEST_ID = '[data-testid="register-link"]';
+export const WALL_LINK_TEST_ID = '[data-testid="wall-link"]';
