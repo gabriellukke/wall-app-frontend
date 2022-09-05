10960f6441ca25b3c2ed5e593b435d2e265128de
gabriellukke <gabriellukke@outlook.com>
2022-09-05 08:05:53 -0300

    chore: tailwind add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     fontFamily: {
       sans: 'source-sans-pro, open-sans, sans-serif',
     },
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: '#000',
+        white: '#fff',
+        eletricblue: '#009cd8',
+        ballblue: '#28a2d1',
+        granite: '#656464',
+      },
+    },
   },
   plugins: [],
 };
